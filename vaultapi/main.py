45c227aac80740d9ab9670038f2d0e999fd7cec5
Vignesh Rao <svignesh1793@gmail.com>
2024-08-18 21:46:56 -0500

    Add CLI functionality Convert to an SDK Add GH workflows

--- vaultapi/main.py
+++ vaultapi/main.py
@@ -15,12 +15,11 @@ def start(**kwargs) -> None:
 
     Keyword Args:
         env_file: Env filepath to load the environment variables.
-        apikey: API Key for authentication.
-        ninja_host: Hostname for the API server.
-        ninja_port: Port number for the API server.
+        apikey: API Key to authenticate the server.
+        secret: Secret access key to access the secret content.
+        host: Hostname for the API server.
+        port: Port number for the API server.
         workers: Number of workers for the uvicorn server.
-        remote_execution: Boolean flag to enable remote execution.
-        api_secret: Secret access key for running commands on server remotely.
         database: FilePath to store the auth database that handles the authentication errors.
         rate_limit: List of dictionaries with ``max_requests`` and ``seconds`` to apply as rate limit.
         log_config: Logging configuration as a dict or a FilePath. Supports .yaml/.yml, .json or .ini formats.
